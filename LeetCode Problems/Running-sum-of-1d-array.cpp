5fc351a594f5ca32ef9288ab31b6a8d3ed608641
Keshav Gupta <59863800+keshavg21@users.noreply.github.com>
2021-05-03 18:56:06 +0530

    Create Running-sum-of-1d-array.cpp

---
+++ LeetCode Problems/Running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+//Problem Link - https://leetcode.com/problems/running-sum-of-1d-array/
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for(int i = 1; i<nums.size(); i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+};
+
+//Time Complexity - O(n) where n is the length of the array.
